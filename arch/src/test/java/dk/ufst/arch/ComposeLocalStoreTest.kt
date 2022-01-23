da135d4828afc1ace937b68cab0f0a80fab0c5a8
patrickusiewicz <patrick.usiewicz@gmail.com>
2022-01-23 21:07:55 +0100

    Observe Compose State through ComposeLocalStore
    The ComposeLocalStore interface is the Compose version of the current LocalStore interface.
    Instead the ComposeLocalStore interface exposes State<T> which can be used in Compose context.

---
+++ arch/src/test/java/dk/ufst/arch/ComposeLocalStoreTest.kt
@@ -0,0 +1,128 @@
+package dk.ufst.arch
+
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.launch
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ComposeLocalStoreTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    private lateinit var globalStore: GlobalStore<AppState, Any, Any>
+
+    @Before
+    fun setup() {
+        setupStore()
+    }
+
+    @Test
+    fun `Test correct recomposition and State data are observed`() {
+        val composedResults = mutableListOf<LocalState>()
+        // Signal an action sent from the Compose scope to the reducer.
+        val signalAction = Channel<Unit>(1)
+
+        composeTestRule.setContent {
+            val localStore: ComposeLocalStore<LocalState, Any> = rememberLocalStore(
+                globalStore,
+                { it.localState.copy() },
+                { it.localState.copy() }
+            )
+
+            composedResults.add(localStore.state.value)
+
+            val scope = rememberCoroutineScope()
+
+            // Wait for outside signal for when to sent an Action.
+            scope.launch {
+                signalAction.consumeEach {
+                    localStore.send(Any())
+                }
+            }
+        }
+
+        composeTestRule.runOnIdle {
+            assertEquals(composedResults.single().value, 1)
+            signalAction.trySend(Unit)
+        }
+
+        composeTestRule.runOnIdle {
+            assertEquals(composedResults.size, 2)
+            assertEquals(composedResults.first().value, 1)
+            assertEquals(composedResults.last().value, 2)
+        }
+    }
+
+    @Test
+    fun `Test no recomposition when forgetting to use copied initial and local values`() {
+        val composedResults = mutableListOf<LocalState>()
+        // Signal an action sent from the Compose scope to the reducer.
+        val signalAction = Channel<Unit>(1)
+
+        composeTestRule.setContent {
+            val localStore: ComposeLocalStore<LocalState, Any> = rememberLocalStore(
+                globalStore,
+                { it.localState },
+                { it.localState }
+            )
+
+            composedResults.add(localStore.state.value)
+
+            val scope = rememberCoroutineScope()
+
+            // Wait for outside signal for when to sent an Action.
+            scope.launch {
+                signalAction.consumeEach {
+                    localStore.send(Any())
+                }
+            }
+        }
+
+        composeTestRule.runOnIdle {
+            assertEquals(composedResults.single().value, 1)
+            signalAction.trySend(Unit)
+        }
+
+        composeTestRule.runOnIdle {
+            assertEquals(composedResults.single().value, 2)
+        }
+    }
+
+    private fun setupStore() {
+        globalStore = GlobalStore(
+            env = Any(),
+            executor = TestExecutor(),
+            initialValue = AppState(),
+            copyValue = { appState ->
+                appState.copy()
+            },
+            reducer = compose(
+                combine(
+                    pullback(
+                        localReducer,
+                        AppState::localState::get,
+                        AppState::localState::set,
+                        { Any() }
+                    )
+                )
+            )
+        )
+    }
+
+    companion object {
+        private data class LocalState(var value: Int = 1)
+        private data class AppState(var localState: LocalState = LocalState())
+
+        private val localReducer: ReducerFunc<LocalState, Any, Any> = { localValue, _, _ ->
+            localValue.value = 2
+            emptyArray()
+        }
+    }
+}
